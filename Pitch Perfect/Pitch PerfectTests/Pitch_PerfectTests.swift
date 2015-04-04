1c6317c24f3b2ade15af9702288877a4e669d948
pbyrne98 <pbyrne98@users.noreply.github.com>
2015-04-04 22:53:52 +1100

    Committed by Udacity

---
+++ Pitch Perfect/Pitch PerfectTests/Pitch_PerfectTests.swift
@@ -0,0 +1,31 @@
+//
+//  Pitch_PerfectTests.swift
+//  Pitch PerfectTests
+//
+//  Created by Paul Byrne on 15/03/2015.
+//  Copyright (c) 2015 Paul Byrne. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Pitch_PerfectTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testExample() {
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        self.measureBlock() {
+        }
+    }
+    
+}
