422e9338ffe518b3515f1f4d5ef5ff2e21fb3094
pbyrne98 <pbyrne98@users.noreply.github.com>
2015-04-04 22:53:47 +1100

    Committed by Udacity

---
+++ Pitch Perfect/Pitch Perfect/AppDelegate.swift
@@ -0,0 +1,38 @@
+//
+//  AppDelegate.swift
+//  Pitch Perfect
+//
+//  Created by Paul Byrne on 15/03/2015.
+//  Copyright (c) 2015 Paul Byrne. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        return true
+    }
+
+    func applicationWillResignActive(application: UIApplication) {
+    }
+
+    func applicationDidEnterBackground(application: UIApplication) {
+    }
+
+    func applicationWillEnterForeground(application: UIApplication) {
+    }
+
+    func applicationDidBecomeActive(application: UIApplication) {
+    }
+
+    func applicationWillTerminate(application: UIApplication) {
+    }
+
+
+}
+
